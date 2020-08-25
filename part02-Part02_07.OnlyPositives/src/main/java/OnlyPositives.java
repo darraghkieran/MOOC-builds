18389a87e4f1ce57882398c0f576e5e44c32bfb7
Darragh Kieran <darraghkieran3@outlook.com>
2020-08-25 19:56:20 +0100

    Create OnlyPositives.java

---
+++ part02-Part02_07.OnlyPositives/src/main/java/OnlyPositives.java
@@ -0,0 +1,24 @@
+
+import java.util.Scanner;
+
+public class OnlyPositives {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+             
+        while (true){
+            System.out.println("Give a number: ");
+            int n = Integer.valueOf(scanner.nextLine());
+            
+            if (n == 0){
+                break;
+            } else if (n<0){
+                System.out.println("Unsuitable number");
+                continue;
+            } 
+            
+            System.out.println(n * n);
+            
+        }
+    }
+}
