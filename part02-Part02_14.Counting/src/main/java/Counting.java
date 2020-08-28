ef0fee612db2e875873856e14fd65d502fc4e668
Darragh Kieran <darraghkieran3@outlook.com>
2020-08-28 11:50:37 +0100

    Create Counting.java

---
+++ part02-Part02_14.Counting/src/main/java/Counting.java
@@ -0,0 +1,17 @@
+
+import java.util.Scanner;
+
+public class Counting {
+    
+    public static void main (String[] args) {
+        Scanner scan = new Scanner (System.in);
+        
+        int x = Integer.valueOf (scan.nextLine());
+        for (int i = x; i <= 100; i++){
+            System.out.println(i);
+        }
+       
+    }
+    
+}
+
