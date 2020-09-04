1ed779b8592dd836e04e57b69fffee80b6af9d94
Darragh Kieran <darraghkieran3@outlook.com>
2020-09-04 20:04:40 +0100

    Create Reprint.java

---
+++ part02-Part02_20.RepeatingBreakingAndRemembering/part02-Part02_22.Reprint/src/main/java/Reprint.java
@@ -0,0 +1,19 @@
+
+import java.util.Scanner;
+
+public class Reprint {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.println("How many times?");
+        int n = Integer.valueOf(scanner.nextLine());
+        for (int i = 0; i < n; i++){
+            printText();
+        }    
+    }
+
+    public static void printText() {
+        System.out.println("In a hole in the ground there lived a method");
+    }    
+}
